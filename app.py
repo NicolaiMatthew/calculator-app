3c082b56175d045eff63fbc6dc1c9f020fbdf07e
Nicolai <nicolairulez@gmail.com>
2024-09-06 20:16:54 -0400

    Got addition to work

--- app.py
+++ app.py
@@ -5,4 +5,13 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 
 def hello_world():
+    if request.method == "POST":
+        # Getting first number
+        first_num = request.form.get('fnum')
+        # Getting second number
+        second_num = request.form.get('snum')
+        # Adding the number together
+        sum = int(first_num) + int(second_num)
+        return "Your total is: %d" % sum
+        
     return render_template('index.html')
